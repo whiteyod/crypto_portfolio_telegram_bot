99f45088dc742acae7dabf7eeac91af48aaea33f
whiteyod <whiteyod@icloud.com>
2026-02-26 13:55:42 +0100

    Refactor sell_buttons initially

--- handlers/buttons/sell_buttons.py
+++ handlers/buttons/sell_buttons.py
@@ -1,6 +1,6 @@
 from aiogram import F, types, Router
 from aiogram.filters.state import State, StatesGroup  
-from assets.db import c, get_ticker_data, conn
+from assets.db import c, get_position, apply_sell
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from aiogram.utils.keyboard import InlineKeyboardBuilder
@@ -9,6 +9,7 @@ from decimal import Decimal
 import sys
 
 from handlers.commands import get_ticker_price
+from services.cmc.CmcClient import get_quotes_usd
 from keyboards import saving_kb, cancel_kb, cancel_kb_market
 
 
@@ -43,8 +44,8 @@ async def sell_button(callback: types.CallbackQuery):
     user_id = callback.from_user.id
     pairs = c.execute(
         '''
-        SELECT pair 
-        FROM messages
+        SELECT symbol 
+        FROM positions
         WHERE user_id = ?
         ''',
         (user_id,)
@@ -97,8 +98,13 @@ async def sell_button(callback: types.CallbackQuery):
 async def create_sell_callback(router, pair, user_id):
     @router.callback_query(F.data  == pair)
     async def sell(callback: types.CallbackQuery, state: FSMContext):
-        price, token_amount = await get_ticker_data(pair, user_id)
+        pos = await get_position(user_id=user_id, symbol=pair)
+        if pos is None:
+            await callback.answer('Position not found', show_alert=True)
+            return
+        token_amount, avg_cost, realized_pnl = map(float, pos)
         market_price = get_ticker_price(pair)
+
         await state.set_state(States.selling_amount_state)
         await callback.message.edit_text(
             f'Selected ticker: <b>{pair}</b>'
@@ -106,17 +112,9 @@ async def create_sell_callback(router, pair, user_id):
             f'\nHow much to sell?',
             reply_markup=cancel_kb()
             )
+
         clear_callback()
         add_callback(pair)
-        
-        # Check data corectness
-        print(
-            f'########## Initial amount: {token_amount*market_price}, price: {price}, tokens: {token_amount}'
-            f'\n########## Current market price: {market_price}'
-            f'\n########## Last profit based on initial amount: {None}'
-        )
-        
-        print(f' >>>>>>>>>>>>>>>>>> {get_callback()}')
 
     return sell
         
@@ -134,94 +132,52 @@ async def sell_amount(message: Message, state=FSMContext):
     # Get input from user
     try:
         selling_amount = float(data['sl_am'])
-        print(
-            f'########## Selling amount from user input: {selling_amount}'
-        )
     except:
         await message.answer(
             'Please send the number without any characters',
             reply_markup=cancel_kb()
             )
-    # Get values from db
-    price, token_amount = await get_ticker_data(pair, user_id)
+    # Get values from db and map data
+    pos = await get_position(user_id, pair)
+    if pos is None:
+        await message.answer('Position not found', reply_markup=saving_kb())
+        await state.clear()
+        return
+
+    token_amount, avg_cost, realized_pnl = map(float, pos)
+
     market_price = get_ticker_price(pair)
-    current_token_amount_price = token_amount * market_price
+    current_value = token_amount * market_price
     
-    # Check data corectness
-    print(
-        f'########## Amount: {current_token_amount_price}, price: {price}'
-        f'\n########## market_price: {market_price}, profit: {current_token_amount_price - (token_amount * price)}'
-    )
 
     # Check if remaining amount is less or equal to selling amount
-    if selling_amount > current_token_amount_price:
-        if current_token_amount_price > 0.1: # If less ask to enter new value
-            await state.update_data(sl_am=message.text)
-            await message.answer(
-                f'You trying to sell <b>{selling_amount}$</b> which is more than your balance of <b>{current_token_amount_price}$</b>'
-                f'\nEnter correct value: ',
-                reply_markup=cancel_kb()
-                )
-        else: 
-            c.execute(
-                '''
-                DELETE FROM messages WHERE user_id = ? AND pair = ?
-                ''',
-                (user_id, pair)
-            )
-            await state.clear()
-            conn.commit()
-            await message.answer(
-                f'{pair} sold at <b>{selling_amount + (current_token_amount_price - (token_amount * price))}$</b> with the total profit of <b>{current_token_amount_price - (token_amount * price)}$</b> and has been removed from the table',
-                reply_markup=saving_kb()
-            )
-    elif (current_token_amount_price - selling_amount) < 0.1 : # If equal ask do user want to sell everything
-        await state.update_data(sl_am=message.text)
-        await state.set_state(States.yes_no_state)
-        # Create inline keyboard to take answers from user
-        kb = InlineKeyboardBuilder()
-        kb.add(
-            types.InlineKeyboardButton(
-                text='Yes',
-                callback_data='answer_yes'
-            ),
-            types.InlineKeyboardButton(
-                text='No',
-                callback_data='answer_no'
-            )
-        )
-        await state.clear()
+    if selling_amount > current_value:
+
         await message.answer(
-            f'Remaining amount of <b>{pair}</b> is equal to the selling amount, do you want to sell everything?'
-            f'\nThe ticker will be deleted from the portfolio.',
-            reply_markup=kb.as_markup()
+            f'You try to sell <b>{selling_amount}$</b> but your balance is <b>{current_value:.2f}$</b>'
+            f'\nEnter correct value: ',
+            reply_markup=cancel_kb()
         )
-    else: ###################################################################################################
-        # Calculating curent profit and amount adjusting
-        
-        token_selling_amount = selling_amount / market_price
-        remaining_tokens = token_amount - token_selling_amount
-        
-        c.execute(
-            '''
-            UPDATE messages SET  token_amount = ?
-            WHERE user_id = ? AND pair = ?
-            ''',
-            (remaining_tokens, user_id, pair)
+        return
+
+    sell_qty = selling_amount / market_price
+
+    try: 
+        await apply_sell(
+            user_id=user_id, symbol=pair, qty=sell_qty, price=market_price
         )
-        conn.commit()
-        await message.answer(
-            f'<b>{pair}</b> sold at <b>{selling_amount}$</b>',
-            reply_markup=saving_kb()
-        )
-        
-        # Check data corectness
-        print(
-            f'########## New amount: {remaining_tokens * market_price}, profit: {(current_token_amount_price - (token_amount * price))}, tokens {remaining_tokens}'
-        )
-        
-        # Reseting the FSM
+    except Exception as e:
+        await message.answer(f'Sell failed: {e}', reply_markup=saving_kb())
         await state.clear()
+        return
+
+    await message.answer(
+        f'<b>{pair}</b> sold at <b>{selling_amount}$</b>',
+        reply_markup=saving_kb()
+    )
+
+    # Reseting the FSM
+    await state.clear()
 
 
 # Handling answer 'Yes'
