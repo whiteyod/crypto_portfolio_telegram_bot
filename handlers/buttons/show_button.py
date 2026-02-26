c8be55f1bd519e847cb9f4217744f38304b0a808
whiteyod <whiteyod@icloud.com>
2026-02-26 14:32:51 +0100

    Refactor show_button

--- handlers/buttons/show_button.py
+++ handlers/buttons/show_button.py
@@ -1,7 +1,6 @@
 from aiogram import Router, F, types
 
-from assets.db import c, get_ticker_data
-from handlers.commands import get_ticker_price
+from assets.db import c
 from keyboards import back_kb
 from services.container import cmc, quotes_cache
 
@@ -31,7 +30,7 @@ async def starting(callback: types.CallbackQuery):
 
 
     # Normalize symbols from DB rows
-    symbols = [''.join(x).upper() for x in rows]
+    symbols = [r[0].upper() for r in rows]
 
     # Send alert if user has no pairs data saved
     if not symbols:
@@ -52,67 +51,77 @@ async def starting(callback: types.CallbackQuery):
             quotes_cache.set(cache_key, quotes)
 
         # Totals for the whole portfolio
+        items = []
         total_current_value = 0.0
         total_cost_value = 0.0
+        total_realized = 0.0
 
         # Duild per-asset info
-        for symbol in symbols:
-            # Get current market price
+        for symbol, qty, avg_cost, realized_pnl in rows:
+            # Convert data values
+            symbol = symbol.upper()
+            qty = float(qty)
+            avg_cost = float(avg_cost)
+            realized_pnl = float(realized_pnl)
+
+            # Get current market price, if none continue
             market_price = quotes.get(symbol)
-            # Send alert if symbol has no price on CMC
             if market_price is None:
-                await callback.answer(
-                    f'{symbol} price not found in CoinMarketCap',
-                    show_alert=True
-                )
                 continue
 
-            # Get amount and mean buy price from DB
-            price, token_amount = await get_ticker_data(symbol, user_id)
-
-            # Calculate values
-            buying_value = token_amount * price
-            current_value = token_amount * market_price
-            current_pnl = current_value - buying_value
+            current_value = qty * market_price
+            cost_value = qty * avg_cost
+            unrealized = current_value - cost_value
+            total = unrealized + realized_pnl
 
             # Update totals
             total_current_value += current_value
             total_cost_value += buying_value
-            allocation = (current_value / total_current_value * 100.0) \
+            total_realized +=realized_pnl
+
+
+            items.append({
+                'symbol': symbol,
+                'qty': qty,
+                'avg_cost': avg_cost,
+                'market_price': market_price,
+                'current_value': current_value,
+                'unrealized': unrealized,
+                'realized': realized_pnl,
+                'total': total,
+            })
+
+        # Pass formatted data with allocation
+        for it in items:
+            # Calculate allocation for each symbol separately
+            allocation = (it['current_value'] / total_current_value * 100.0) \
                 if total_current_value else 0.0
-
-            # Format values
-            formatted_current_value = '{:.2f}'.format(current_value)
-            formatted_buy_price = '{:.9f}'.format(price)
-            formatted_pnl = float('{:.2f}'.format(current_pnl))
-
+            # Create message for each symbol
             portfolio_info.append(
-                f'<b>{symbol} {round(token_amount, 2)} ({formatted_current_value} USD)</b>'
-                f'\nMean buying price: <b>{formatted_buy_price}$</b>'
-                f'\nP&L: {"+" if formatted_pnl > 0 else ""}{formatted_pnl}$'
-                f'\nAllocation: {allocation}%'
+                f'<b>{it["symbol"]} {it["qty"]} ({it["current_value"]:.2f}USD)</b>'
+                f'\nAvg cost: <b>{it["avg_cost"]:.9f}$</b>'
+                f'\nMarket: <b>{it["market_price"]:.9f}$</b>'
+                f'\nUnrealized P/L: {it["unrealized"]:+.2f}$'
+                f'\nRealized P/L: {it["realized"]:+.2f}$'
+                f'\nTotal P/L: {it["total"]:+.2f}$'
+                f'\nAllocation: <b>{allocation:.2f}%</b>'
                 f'\n{chr(8212) * 13}'
             )
 
         # Add totals footer
-        total_pnl = total_current_value - total_cost_value
+        total_unrealized = (total_current_value - total_cost_value)
+        total_pnl = total_unrealized + total_realized
         total_pnl_pct = (total_pnl / total_cost_value * 100.0)\
             if total_cost_value else 0.0
 
         portfolio_info.append(
-            f'\n<b>Total value:</b> {total_cost_value:.2f} USD'
+            f'\n<b>Total value:</b> {total_current_value:.2f} USD'
+            f'\n<b>Unrealized P/L:</b> {total_unrealized:+.2f} USD'
+            f'\n <b>Realized P/L:</b> {total_realized:+.2f}'
             f'\n<b>Total P/L:</b> {total_pnl:+.2f} USD ({total_pnl_pct:+.2f}%)'
         )
-    c.execute(
-        '''
-        SELECT pair, price, token_amount
-        FROM messages
-        WHERE user_id = ?
-        ''',
-              (user_id,))
-    results = c.fetchall()
 
-    if results:
+    if rows:
         # Sending portfolio as a message
         edited_portfolio = '\n'.join(portfolio_info)
         await callback.message.edit_text(
